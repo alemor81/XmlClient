652217e886e9f91cec2d0f8e6001e2476f2d2ff9
Alessio Morzillo <alessio.morzillo@gmail.com>
2012-02-29 15:23:43 +0100

    first commit

---
+++ Resources/app.js
@@ -0,0 +1,33 @@
+var win = Titanium.UI.createWindow({
+    backgroundColor:'#fff'
+});
+
+var data = [];
+var table = Ti.UI.createTableView();
+win.add(table);
+
+var url="http://apod.nasa.gov/apod.rss"; //  rss feed url
+var xhr = Titanium.Network.createHTTPClient();
+
+xhr.onload = function() {
+	// Data is returned from the blog, start parsing
+	var doc = this.responseXML.documentElement;
+
+	// begin looping through blog posts
+	var items = doc.getElementsByTagName("item");
+	for (var i=0;i<items.length;i++) {
+		data.push({
+			title: items.item(i).getElementsByTagName("title").item(0).text
+		});
+	}
+	table.data = data;
+};
+xhr.onerror = function(e) {
+	// should do something more robust
+	alert('Network error '+e.error);
+};
+
+xhr.open('GET',url);
+xhr.send();
+
+win.open();
