8284a26963c8a63883ff0ed997ee7c0e14db74d4
Alessio Morzillo <alessio.morzillo@gmail.com>
2012-02-29 16:46:01 +0100

    nuovo commit

--- Resources/app.js
+++ Resources/app.js
@@ -13,14 +13,13 @@ xhr.onload = function() {
 	// Data is returned from the blog, start parsing
 	var doc = this.responseXML.documentElement;
 
-	// begin looping through blog posts
 	var items = doc.getElementsByTagName("item");
 	for (var i=0;i<items.length;i++) {
 		data.push({
 			title: items.item(i).getElementsByTagName("title").item(0).text
 		});
 	}
-	table.data = data;
+	table.data = data; 
 };
 xhr.onerror = function(e) {
 	alert('Network error '+e.error);
